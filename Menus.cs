45f766a4814522d9a592ae30736cf3660ec63bbd
dajolol <dajolol88@gmail.com>
2023-12-08 04:14:57 +0100

    Third commit, try

--- Menus.cs
+++ Menus.cs
@@ -11,7 +11,7 @@ public class Menus
         Console.WriteLine("Scroll through the menu by using the arrow keys,");
         Console.WriteLine("press Enter to choose the option you've selected");
         Console.WriteLine("------------------------------------------------");
-        int index = 0;
+        int index = 1;
         string[] mainMenu = { "1. Search and sort by category in the library", "2. Handle games", "3. Handle Consoles", "4. Handle Genres", "5. Exit program" };
 
         Console.WriteLine("\n################################################");
