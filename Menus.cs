7c265d460a6aef94c0117973df32c7c341bdf3f8
dajolol <dajolol88@gmail.com>
2023-12-09 04:43:21 +0100

    Removed comments regarding previous code

--- Menus.cs
+++ Menus.cs
@@ -2,6 +2,7 @@ using System.Transactions;
 
 public class Menus
 {
+    private string currentMarker = "> \u001b[32m";
     public static void PrintMainMenu(int option, string currentMarker)
     {
         Console.Clear();
@@ -14,29 +15,6 @@ public class Menus
         string[] menu = { "1. Search and sort by category in the library", "2. Handle games", "3. Handle Consoles", "4. Handle Genres", "5. Exit program" };
 
         ColorMenuText(currentMarker, menu, option);
-        // for (int i = 1; i <= menu.Length; i++)
-        // {
-        //     if (option == i)
-        //     {
-        //         Console.WriteLine($"{currentMarker}{menu[i - 1]}\u001b[0m");
-        //     }
-        //     else
-        //     {
-        //         Console.WriteLine($"{menu[i - 1]}  \u001b[0m");
-        //     }
-        // }
-        // foreach (string menuOption in mainMenu)
-        // {
-        //     i++;
-        //     if (option == i)
-        //     {
-        //         Console.WriteLine($"{currentMarker}{menuOption}\u001b[0m");
-        //     }
-        //     else
-        //     {
-        //         Console.WriteLine($"{menuOption}  \u001b[0m");
-        //     }
-        // }
 
         Console.WriteLine("\n################################################");
         Console.ResetColor();
--- Program.cs
+++ Program.cs
@@ -18,12 +18,6 @@ internal class Program
 
             Menus.PrintMainMenu(option, currentMarker);
 
-            // Console.WriteLine($"{(option == 1 ? currentMarker : "  ")}1. Search and sort by category in the library\u001b[0m");
-            // Console.WriteLine($"{(option == 2 ? currentMarker : "  ")}2. Handle games\u001b[0m");
-            // Console.WriteLine($"{(option == 3 ? currentMarker : "  ")}3. Handle Consoles\u001b[0m");
-            // Console.WriteLine($"{(option == 4 ? currentMarker : "  ")}4. Handle Genres\u001b[0m");
-            // Console.WriteLine($"{(option == 5 ? currentMarker : "  ")}5. Exit program\u001b[0m");
-
             keyPressed = Console.ReadKey(false);
 
             switch (keyPressed.Key)
