3da3c0f61a332da7b56ff6761b206c97b739411a
dajolol <dajolol88@gmail.com>
2023-12-13 04:23:15 +0100

    changed field name

--- Menus.cs
+++ Menus.cs
@@ -2,7 +2,7 @@ using System.Transactions;
 
 public class Menus
 {
-    static readonly string currentMarker = "> \u001b[32m";
+    static readonly string currentMenuMarker = "> \u001b[32m";
     public static void PrintMainMenu(int option)
     {
         Console.Clear();
@@ -26,7 +26,7 @@ public class Menus
         {
             if (option == i)
             {
-                Console.WriteLine($"{currentMarker}{menu[i - 1]}\u001b[0m");
+                Console.WriteLine($"{currentMenuMarker}{menu[i - 1]}\u001b[0m");
             }
             else
             {
