dace9615751eee20454cf187cc204ebcadd0e871
dajolol <dajolol88@gmail.com>
2023-12-28 01:34:52 +0100

    Added packages Data.SqlClient and Dapper as well as established connection to database

--- SqlFunctions.cs
+++ SqlFunctions.cs
@@ -1,4 +1,15 @@
+using System.Data;
+using System.Data.SqlClient;
+using Dapper;
 class SqlFunctions
 {
+    static IDbConnection connection = new SqlConnection("Server=localhost,1433;User=sa;Password=apA123!#!;Database=GameLibrary;");
+
+    private static void Open()
+    {
+        if (connection.State != ConnectionState.Open)
+            connection.Open();
+    }
+
 
 }
